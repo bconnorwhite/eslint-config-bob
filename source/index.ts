0311a17001355fa6754808c41bdbdcfae31a1776
Connor White <connor.bcw@gmail.com>
2020-09-24 21:35:08 -0500

    fix: turn off react/display-name

--- source/index.ts
+++ source/index.ts
@@ -37,6 +37,7 @@ const config: ESLintConfig = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
     "@typescript-eslint/no-use-before-define": ["error", { "functions": false }],
+    "react/display-name": "off",
     "react/prop-types": "off",
     "no-await-in-loop": "warn",
     "no-console": ["error", { "allow": ["warn", "error", "info"] }],
