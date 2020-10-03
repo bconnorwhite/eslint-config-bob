6116eeafd5f80dd2c3ea9c0c8567a97cc0c2b6a8
Connor White <connor.bcw@gmail.com>
2020-10-02 19:31:19 -0500

    feat: added react/jsx-wrap-multilines

--- source/rules/react.ts
+++ source/rules/react.ts
@@ -3,6 +3,15 @@ import { ESLintRules } from "types-eslintrc";
 const rules: ESLintRules = {
   "react/display-name": "off",
   "react/jsx-pascal-case": "error",
+  "react/jsx-wrap-multilines": ["error", {
+    declaration: "parens-new-line",
+    assignment: "parens-new-line",
+    return: "parens-new-line",
+    arrow: "parens-new-line",
+    condition: "ignore",
+    logical: "ignore",
+    prop: "ignore"
+  }],
   "react/prop-types": "off"
 }
 
