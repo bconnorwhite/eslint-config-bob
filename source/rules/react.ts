ef4145bc819123019cef425a74967eb290ab886c
Connor White <connor.bcw@gmail.com>
2020-10-02 19:30:05 -0500

    refactor: separate rules by plugin

---
+++ source/rules/react.ts
@@ -0,0 +1,8 @@
+import { ESLintRules } from "types-eslintrc";
+
+const rules: ESLintRules = {
+  "react/display-name": "off",
+  "react/prop-types": "off"
+}
+
+export default rules;
---
+++ source/rules/typescript.ts
@@ -0,0 +1,20 @@
+import { ESLintRules } from "types-eslintrc";
+
+const rules: ESLintRules = {
+  "@typescript-eslint/camelcase": "off",
+  "@typescript-eslint/explicit-function-return-type": "off",
+  "@typescript-eslint/explicit-module-boundary-types": "off",
+  "@typescript-eslint/no-empty-function": "off",
+  "@typescript-eslint/no-empty-interface": ["error", {
+    allowSingleExtends: true
+  }],
+  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/no-unused-vars": ["warn", {
+    argsIgnorePattern: "^_"
+  }],
+  "@typescript-eslint/no-use-before-define": ["error", {
+    functions: false
+  }]
+}
+
+export default rules;
